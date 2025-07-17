2827ad687b8936cb3eacb2985cec8760fbad662f
Mostakim15 <mbmostakim5050@gmail.com>
2025-07-17 22:13:54 +0600

    Update full project

--- show_cv/views.py
+++ show_cv/views.py
@@ -3,4 +3,11 @@ from cv_app_1.models import CV
 # Create your views here.
 def show(request):
     cvs = CV.objects.all()
+    if request.method == "GET":
+        search_query = request.GET.get("Search", "")
+        if search_query:
+            cvs = cvs.filter(name__icontains=search_query)
+    else:
+        cvs = CV.objects.all()
+    # Render the show template with the list of CVs
     return render(request, 'show/show.html', {'cvs': cvs})
